13c9fb9b4382f7ea4fe0b913d6dbfc83526658de
notdaan <Danieljohnmc1@gmail.com>
2023-02-05 21:42:09 +1100

    added functionallity
    added in a expand and refresh button on the editor input.
    
    also adjusted some layout issues and bug fixing

--- src/Body.js
+++ src/Body.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import MarkdownPreview from "@uiw/react-markdown-preview";
 import { ReactMarkdownProps } from 'react-markdown';
+import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
 type MarkdownPreviewProps = {
   className: 'preview'
 } & ReactMarkdownProps
 
-let source = `
-## MarkdownPreview
-
-> todo: React component preview markdown text.
-`;
 export default class Header extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      markdown: "hi my name is daniel",
+      markdown: "",
+      editorToggle: false,
     };
   }
   updateMarkdown(markdown) {
     this.setState({markdown});
   };
   
+  ToggleEditor(editorToggle) {
+    this.setState({
+      editorToggle: !this.state.editorToggle
+    })
+  }
+
+  refreshMarkdown(markdown) {
+    this.setState({
+      markdown: "",
+    })
+  }
+
     render(){
       return (
         <body>
           <div className='body-container'>
             <div className='editor-container '>
+              <div className='editor-header'>
+              <MDBBtn id='toggle-size-btn' tag='a' color='none' className='m-1' style={{ color: '#47225c' }} onClick={this.refreshMarkdown.bind(this)}>
+                <MDBIcon fas icon="sync" size='2x'/>
+              </MDBBtn>
               <h1 className='body-heading'>Editor</h1>
-              <div className='text-area'>
+              <MDBBtn id='toggle-size-btn' tag='a' color='none' className='m-1' style={{ color: '#47225c' }} onClick={this.ToggleEditor.bind(this)}>
+                <MDBIcon fas icon={this.state.editorToggle ? "expand" : "compress"} size='2x'/>
+              </MDBBtn>
+              </div>
+              <div className={'text-area-' + (this.state.editorToggle ? 'close' : 'open')}>
                 <textarea 
                 id='input' 
                 className='input' 
@@ -42,7 +59,7 @@ export default class Header extends React.Component{
             <div className='line-break'></div>
             <div className='preview-container'>
                 <h1 className='body-heading'>Preview</h1>
-                <div className='text-area 'data-color-mode="light">
+                <div className='text-area' data-color-mode="light">
                   <MarkdownPreview source={this.state.markdown} />
                 </div>
             </div>
